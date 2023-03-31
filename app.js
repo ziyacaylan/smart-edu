542590d4dd1da000de00ce61a854385891116cf8
ziyacaylan <ziyacaylan@gmail.com>
2023-03-31 17:11:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("INDEX SAYFASI");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
